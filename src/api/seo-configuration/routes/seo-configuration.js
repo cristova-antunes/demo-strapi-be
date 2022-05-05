548c4401a695ff1ef3c538871cf538677f0f440d
Cristóvão Antunes <cristovao.av.antunes@gmail.com>
2022-05-05 16:48:12 +0100

    normalize demo files

--- src/api/seo-configuration/routes/seo-configuration.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * seo-configuration router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::seo-configuration.seo-configuration');
--- src/api/seo-configuration/services/seo-configuration.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * seo-configuration service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::seo-configuration.seo-configuration');
